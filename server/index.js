5d912b792074afbb5dd296d7890b7478407ceb2e
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2025-08-19 14:47:09 -0300

    Crud feito

--- server/index.js
+++ server/index.js
@@ -18,5 +18,77 @@ app.post("/api/book", async (req, res) => {
   res.json({ message: "Livro cadastrado com sucesso!", data });
 });
 
+// Rota para listar livros
+app.get("/api/book", async (req, res) => {
+  const { data, error } = await supabase
+    .from("book")
+    .select("*")
+    .order("id", { ascending: false });
+  if (error) return res.status(400).json({ error: error.message });
+  res.json(data);
+});
+
+// Rota para buscar livro por ID
+app.get("/api/book/:id", async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("book")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) return res.status(400).json({ error: error.message });
+  res.json(data);
+});
+
+// Rota para atualizar livro
+app.put("/api/book/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, author, number_of_pages, year_of_publication } = req.body
+  const { data, error } = await supabase
+    .from("book")
+    .update({ title, author, number_of_pages, year_of_publication })
+    .eq("id", id);
+  if (error) return res.status(400).json({ error: error.message });
+  res.json({ message: "Livro atualizado com sucesso!", data });
+});
+
+// Rota para deletar livro
+app.delete("/api/book/:id", async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("book")
+    .delete()
+    .eq("id", id);
+  if (error) return res.status(400).json({ error: error.message });
+  res.json({ message: "Livro deletado com sucesso!", data });
+});
+
+// Cadastro
+app.post("/api/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const { data, error } = await supabase.auth.signUp({ email, password });
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json({ message: "Usuário cadastrado com sucesso!", user: data.user });
+});
+
+// Login
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json({ message: "Login realizado com sucesso!", session: data.session });
+});
+  
+// Logout
+app.post("/api/logout", async (req, res) => {
+  const { error } = await supabase.auth.signOut();
+  if (error) return res.status(400).json({ error: error.message });
+  res.json({ message: "Logout realizado!" });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
